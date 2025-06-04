ff6241e139e03bf337af3174e30069198fd63232
David71116 <david984yeh@gmail.com>
2025-06-04 19:50:31 +0800

    Add files via upload

---
+++ ContactFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace 期末.Models
+{
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = "請輸入名字")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "請輸入 Email")]
+        [EmailAddress(ErrorMessage = "Email 格式不正確")]
+        public string Email { get; set; }
+    }
+}
